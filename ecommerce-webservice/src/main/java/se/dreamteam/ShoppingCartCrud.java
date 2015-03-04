584cc0572854c1315e7a669f008d1cc638f60434
Stefan De Geer <sdgkeff@gmail.com>
2015-03-04 17:39:41 +0100

    går nu att hämta alla produkter i en shoppingcart

---
+++ ecommerce-webservice/src/main/java/se/dreamteam/ShoppingCartCrud.java
@@ -0,0 +1,64 @@
+package se.dreamteam;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import se.dreamteam.ecommerce.ECommerceManager;
+import se.dreamteam.ecommerce.repository.sqldatabase.SqlOrderRepository;
+import se.dreamteam.ecommerce.repository.sqldatabase.SqlProductRepository;
+import se.dreamteam.ecommerce.repository.sqldatabase.SqlShoppingcartRepository;
+import se.dreamteam.ecommerce.repository.sqldatabase.SqlUserRepository;
+
+@Path("users/{userId}/shoppingcart")
+// @Consumes(MediaType.APPLICATION_JSON)
+// @Produces(MediaType.APPLICATION_JSON)
+public final class ShoppingCartCrud
+{
+	private SqlOrderRepository orders = new SqlOrderRepository();
+	private SqlProductRepository products = new SqlProductRepository();
+	private SqlUserRepository users = new SqlUserRepository();
+	private SqlShoppingcartRepository shoppingcart = new SqlShoppingcartRepository();
+	
+	private final ECommerceManager manager = new ECommerceManager(orders, products, users,shoppingcart);
+	@Context
+	public UriInfo uriInfo;
+
+	@GET
+	public final Response getShoppingCart(@PathParam("userId") final String username)
+	{
+		return Response.ok(manager.getShoppingCart(username)).build();
+		//return Response.ok(username + ", ShoppingCart").build();
+	}
+
+	@POST
+	public final Response addProductToShoppingCart(@PathParam("userId") final String username, final int productId)
+	{
+		// final URI location =
+		// uriInfo.getAbsolutePathBuilder().path(manager.addProductToShoppingCart(productId)).build();
+		// return Response.created(location).build();
+		return Response.ok(username + ", Nytt item i ShoppingCart: " + productId).build();
+	}
+
+	@DELETE
+	@Path("{productId}")
+	public final Response removeProductFromShoppingCart(@PathParam("userId") final String username, @PathParam("productId") final String productId)
+	{
+		// return Response.ok(manager.removeProductFromShoppingCart(productId,
+		// username)).build();
+		return Response.ok(username + ", ta bort item från ShoppingCart: " + productId).build();
+	}
+
+	@DELETE
+	public final Response removeAllProductsFromShoppingCart(@PathParam("userId") final String username)
+	{
+		// return
+		// Response.ok(manager.removeAllProductsFromShoppingCart(username)).build();
+		return Response.ok(username + ", ta bort alla items från ShoppingCart").build();
+	}
+}
---
+++ ecommerce-webservice/src/main/java/se/dreamteam/mappers/IntegerWriter.java
@@ -0,0 +1,88 @@
+package se.dreamteam.mappers;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.TreeSet;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import com.google.gson.stream.JsonWriter;
+
+@Provider
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public final class IntegerWriter implements MessageBodyWriter<TreeSet<Integer>>
+{
+	private Gson gson;
+
+	public IntegerWriter()
+	{
+		gson = new GsonBuilder().registerTypeAdapter(TreeSet.class, new ProductAdapter()).create();
+	}
+
+	// MessageBodyWriter
+	@Override
+	public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType)
+	{
+		return type.isAssignableFrom(TreeSet.class);
+	}
+
+	@Override
+	public long getSize(TreeSet<Integer> integers, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType)
+	{
+		return 0;
+	}
+
+	@Override
+	public void writeTo(TreeSet<Integer> products, Class<?> type, Type genericType, Annotation[] annotations,
+			MediaType mediaType, MultivaluedMap<String, Object> httpHeaders,
+			OutputStream entityStream)
+			throws IOException, WebApplicationException
+	{
+		try (final JsonWriter writer = new JsonWriter(new OutputStreamWriter(entityStream)))
+		{
+			gson.toJson(products, TreeSet.class, writer);
+		}
+	}
+
+	private static final class ProductAdapter implements JsonSerializer<TreeSet<Integer>>
+	{
+
+		@Override
+		public JsonElement serialize(TreeSet<Integer> productIds, Type typeOfSrc, JsonSerializationContext context)
+		{
+			//The Object which will be returned
+			final JsonObject jsonToReturn = new JsonObject();
+			//An array to hold all products
+			final JsonArray jsonArrayForIntegers = new JsonArray();
+			
+			for(Integer integer: productIds)
+			{
+				jsonArrayForIntegers.add(new JsonPrimitive(Integer.toString(integer)));
+			}
+			//Adding the array to the jsonReturn-object
+			jsonToReturn.add("productIds", jsonArrayForIntegers);
+			
+			return jsonToReturn;
+		}
+
+	}
+
+}

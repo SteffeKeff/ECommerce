6b2d28dd34815c46564daa11d1105875bb73762c
Maximilian Kern <maxkernw@gmail.com>
2015-03-04 14:00:51 +0100

    Orderrepo-createOrder

--- ecommerce-service/src/main/java/se/dreamteam/ecommerce/repository/sqldatabase/SqlOrderRepository.java
+++ ecommerce-service/src/main/java/se/dreamteam/ecommerce/repository/sqldatabase/SqlOrderRepository.java
@@ -3,11 +3,14 @@ package se.dreamteam.ecommerce.repository.sqldatabase;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import se.dreamteam.ecommerce.exceptions.RepositoryException;
 import se.dreamteam.ecommerce.repository.sqlinterface.SqlOrderInterface;
 import se.dreamteam.model.Order;
+import se.dreamteam.model.Product;
 import se.dreamteam.model.User;
 
 public class SqlOrderRepository implements SqlOrderInterface
@@ -18,13 +21,34 @@ public class SqlOrderRepository implements SqlOrderInterface
 	private final static String PASSWORD = "dr3amt3am";
 
 	@Override
-	public Order createOrder(User user, Order order) throws RepositoryException
+	public String createOrder(User user, Order order, Product product) throws RepositoryException
 	{
 		try (final Connection con = getConnection())
 		{
-			try (PreparedStatement stmt = con.prepareStatement("INSERT INTO dreamteam.Orders VALUES (null,null,?);"
-					+ "											INSERT INTO dreamteam.UserHasOrder VALUES(null,?,?)"))
+			try (PreparedStatement stmt = con.prepareStatement("INSERT INTO dreamteam.Orders VALUES (null,null,?);", Statement.RETURN_GENERATED_KEYS))
 			{
+				stmt.setInt(1, 1);
+				int affectedRows = stmt.executeUpdate();
+				if (affectedRows == 1)
+				{
+					ResultSet key = stmt.getGeneratedKeys();
+					
+					try (PreparedStatement secondStmt = con.prepareStatement("INSERT INTO dreamteam.UserHasOrder (userid,orderid) VALUES(null,?,?)"))
+					{
+						secondStmt.setInt(1, user.getId());
+						secondStmt.setInt(2, key.getInt(1));
+						secondStmt.executeUpdate();
+						try (PreparedStatement thirdStmt = con.prepareStatement("INSERT INTO dreamteam.UserHasProducts(userid, productid) VALUES (null,?,?)"))
+						{
+							thirdStmt.setInt(1, user.getId());
+							thirdStmt.setInt(2, product.getId());
+							thirdStmt.executeUpdate();
+							return Integer.toString(key.getInt(1));
+						}
+
+					}
+
+				}
 
 			}
 
--- ecommerce-service/src/main/java/se/dreamteam/ecommerce/repository/sqlinterface/SqlOrderInterface.java
+++ ecommerce-service/src/main/java/se/dreamteam/ecommerce/repository/sqlinterface/SqlOrderInterface.java
@@ -2,11 +2,12 @@ package se.dreamteam.ecommerce.repository.sqlinterface;
 
 import se.dreamteam.ecommerce.exceptions.RepositoryException;
 import se.dreamteam.model.Order;
+import se.dreamteam.model.Product;
 import se.dreamteam.model.User;
 
 public interface SqlOrderInterface
 {
-	Order createOrder(User user, Order order) throws RepositoryException;
+	String createOrder(User user, Order order, Product product) throws RepositoryException;
 
 	Order getAllOrders(Order order) throws RepositoryException;
 

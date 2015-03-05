8bac1cb342ee110d58e14c124b307c0e0c11a43d
Stefan De Geer <sdgkeff@gmail.com>
2015-03-05 17:34:45 +0100

    nu går det att skapa en order! tjohej.

--- ecommerce-service/src/main/java/se/dreamteam/ecommerce/repository/sqlinterface/SqlOrderInterface.java
+++ ecommerce-service/src/main/java/se/dreamteam/ecommerce/repository/sqlinterface/SqlOrderInterface.java
@@ -1,13 +1,15 @@
 package se.dreamteam.ecommerce.repository.sqlinterface;
 
+import java.util.ArrayList;
 import java.util.TreeSet;
 
 import se.dreamteam.ecommerce.exceptions.RepositoryException;
 import se.dreamteam.model.Order;
+import se.dreamteam.model.Product;
 
 public interface SqlOrderInterface
 {
-	String createOrder(String username) throws RepositoryException;
+	String createOrder(String username, ArrayList<Integer> products) throws RepositoryException;
 
 	TreeSet<Order> getAllOrders(String username) throws RepositoryException;
 
